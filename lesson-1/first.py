32c6a350266e301ba26c0b0dcb1ff03960397bf5
kaZuhacodes <blackpanther20072727@gmail.com>
2025-02-16 11:54:54 +0500

    Hello

---
+++ lesson-1/first.py
@@ -0,0 +1,20 @@
+print("Hello, world!")
+
+a = 1
+b = 2
+'''
+a + b
+a - b
+a * b
+a / b
+a // b
+a % b
+a ** b
+'''
+divmod(a, b)
+x,y = divmod(a, b)
+print(x, y) 
+
+a = int(input("a = "))
+b = int(input("b = "))
+print(a + b)
